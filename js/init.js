082f22e70e2a428369d76101346241bfe49b23a5
AdrianMM1 <87627537+AdrianMM1@users.noreply.github.com>
2021-07-19 14:16:19 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyA7dG0RtbPN24dq6QnwyKD4jJKB_HLIjeQ",
+    authDomain: "dwpamm-53b4c.firebaseapp.com",
+    projectId: "dwpamm-53b4c",
+    storageBucket: "dwpamm-53b4c.appspot.com",
+    messagingSenderId: "1038798354862",
+    appId: "1:1038798354862:web:c63a7b42550f9cf1a2303f",
+    measurementId: "G-YRJZRQNFJF"
+});
